877db3a0b1bb74c0410bd9aa3db4a9a0f49fcef6
appkins <nbatkins@gmail.com>
2019-05-22 18:47:27 -0500

    project restructuring

---
+++ include/cpp_redis/helpers/string_util.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Nick on 4/24/19.
+//
+
+#ifndef CPP_REDIS_STRING_UTIL_H
+#define CPP_REDIS_STRING_UTIL_H
+
+#endif // CPP_REDIS_STRING_UTIL_H
---
+++ include/cpp_redis/misc/crc16.hpp
@@ -0,0 +1,8 @@
+//
+// Created by Nick on 4/24/19.
+//
+
+#ifndef CPP_REDIS_CRC16_H
+#define CPP_REDIS_CRC16_H
+
+#endif // CPP_REDIS_CRC16_H
